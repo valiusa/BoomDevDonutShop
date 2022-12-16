977d3255c50ffbebd7d6d0ede55d0c2286e845d9
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-16 23:08:55 +0200

    donut shop start page done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-//import logo from './logo.svg';
+import StartComponent from "./components/StartComponent";
+
+// styles
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <section class="hero">
-                <div class="hero-body">
-                    <p class="title">A React Task</p>
-                    <p class="subtitle">by Boom.dev</p>
-                </div>
-            </section>
-            <div class="container is-fullhd">
-                <div class="notification">
-                    Edit the <code>./src</code> folder to add components.
-                </div>
-            </div>
+            <StartComponent></StartComponent>
         </div>
     );
 }
