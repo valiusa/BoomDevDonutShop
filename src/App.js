bec854db5219795084815f133a5018f71d3f5a41
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-16 18:36:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section class="hero">
+                <div class="hero-body">
+                    <p class="title">A React Task</p>
+                    <p class="subtitle">by Boom.dev</p>
+                </div>
+            </section>
+            <div class="container is-fullhd">
+                <div class="notification">
+                    Edit the <code>./src</code> folder to add components.
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
